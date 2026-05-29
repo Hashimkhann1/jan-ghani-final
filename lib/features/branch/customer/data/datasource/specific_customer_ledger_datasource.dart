4bda0c39c85d317e74d6d3b11ee8839cc07df4cd
Shahabmustafa <shahabmustafa57@gmail.com>
2026-05-29 14:57:23 +0500

    Add Vercel Flutter build config

---
+++ lib/features/branch/customer/data/datasource/specific_customer_ledger_datasource.dart
@@ -0,0 +1,22 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../model/specific_customer_ledger_model.dart';
+
+class CustomerLedgerDatasource {
+  final _client = Supabase.instance.client;
+
+  Future<List<SpecificCustomerLedgerModel>> getByCustomer({
+    required String customerId,
+  }) async {
+    final result = await _client
+        .from('customer_ledger')
+        .select()
+        .eq('customer_id', customerId)
+        .isFilter('deleted_at', null)
+        .order('created_at', ascending: false);
+
+    return (result as List)
+        .map((r) => SpecificCustomerLedgerModel.fromMap(r))
+        .toList();
+  }
+}
