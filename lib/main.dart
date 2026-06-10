78b8c9a28d419e6305bee158a33e8996ec4c7a04
Shahabmustafa <shahabmustafa57@gmail.com>
2026-06-10 10:48:28 +0500

    update the code and changes on accoutant

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-// import 'dart:io';
-// import 'dart:io';
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:jan_ghani_final/core/service/db/db_service.dart';
 import 'package:jan_ghani_final/core/theme/light_theme.dart';
 import 'package:jan_ghani_final/features/accountant/authentication/presentation/screen/login_screen.dart';
 import 'package:jan_ghani_final/features/accountant/dashboard/presentation/screen/dashboard_screen.dart';
@@ -13,13 +9,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../core/config/store_config.dart';
 import '../core/service/session/accountant_session.dart';
-import '../core/service/sync/sync_service.dart';
 import '../features/branch/authentication/presentation/provider/auth_provider.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-import 'core/widget/sidebar/branch_sidebar_widget.dart';
+import 'core/service/db/db_service.dart';
+import 'core/service/sync/sync_service.dart';
 import 'features/accountant/authentication/presentation/providers/accoutant_session_provider.dart';
-import 'features/branch/authentication/presentation/screen/login_screen.dart';
 
 
 final supabase = Supabase.instance.client;
@@ -39,11 +32,10 @@ void main() async{
   );
   await SharedPreferences.getInstance();
   await StoreConfig.load();
-  // if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-  //   SyncService().start();
-  //   DataBaseService.getConnection();
-  // }
-
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    SyncService().start();
+    DataBaseService.getConnection();
+  }
   runApp(ProviderScope(child: MyApp()));
 }
 
